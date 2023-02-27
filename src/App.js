94e7a96c9037a167673533835afe3ababb72b718
Nishant <nishant203@gmail.com>
2023-02-27 19:28:42 +0530

    Change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Small change
+          Another change
         </p>
         <a
           className="App-link"
